9986e8cd0e06924b4fadb19530cd9aa939ef1a05
ggEz75 <guillegomez575@gmail.com>
2024-10-28 16:24:35 -0300

    modificacion 28/10/24

---
+++ src/pages/CreateProduct.jsx
@@ -0,0 +1,14 @@
+import { React } from "react";
+
+const Formulario = () => {
+
+
+    
+    return(
+        <div>
+            <h1>Formulario</h1>
+        </div>
+    );
+}
+
+export default Formulario;
---
+++ src/pages/List.jsx
@@ -0,0 +1,14 @@
+import { React } from "react";
+
+const List = () => {
+
+
+    
+    return(
+        <div>
+            <h1>Listar productos</h1>
+        </div>
+    );
+}
+
+export default List;
